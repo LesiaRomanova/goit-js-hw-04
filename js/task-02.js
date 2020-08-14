c2b5e3f3c49d38cf9e368254c5978d33127a2f5e
Romanovalm85@gmail.com <you@example.com>
2020-08-14 23:24:29 +0300

    M-4

---
+++ js/task-02.js
@@ -0,0 +1,81 @@
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action.call(inventory, itemName);
+};
+
+invokeInventoryAction("Medkit", inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction("Gas mask", inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
+
+// const inventory = {
+//   items: ["Монорельса", "Фильтр"],
+//   add(itemName) {
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     this.items = this.items.filter((item) => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryOperation = function (itemName, inventoryAction) {
+//   console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+//   inventoryAction.call(inventory, itemName);
+// };
+
+// invokeInventoryOperation("Аптечка", inventory.add);
+// // Invoking add opeartion on Аптечка
+
+// console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+// invokeInventoryOperation("Фильтр", inventory.remove);
+// // Invoking remove opeartion on Фильтр
+
+// console.log(inventory.items); // ['Монорельса', 'Аптечка']
+
+// const inventory = {
+//   items: ["Монорельса", "Фильтр"],
+//   add(itemName) {
+//     this.items.push(itemName);
+//   },
+//   remove(itemName) {
+//     this.items = this.items.filter((item) => item !== itemName);
+//   },
+// };
+
+// const invokeInventoryOperation = function (itemName, inventoryAction) {
+//   console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+//   inventoryAction.call(inventory, itemName);
+// };
+
+// invokeInventoryOperation("Аптечка", inventory.add);
+// // Invoking add opeartion on Аптечка
+
+// console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+// invokeInventoryOperation("Фильтр", inventory.remove);
+// // Invoking remove opeartion on Фильтр
+
+// console.log(inventory.items); // ['Монорельса', 'Аптечка']
